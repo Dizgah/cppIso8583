af0e85014a1af53537fa267d17cc5d40bacae540
Mahmoud Hosseinipour <mhdizgah@gmail.com>
2023-08-23 16:08:50 +0330

    modyfing cmake file

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <system_error>
+#include <map>
+#include <utility>
+#include <chrono>
+
+#include "cppiso8583.h"
+
+#include "dl_iso8583.h"
+#include "dl_iso8583_defs_1987.h"
+#include "dl_output.h"
+
+
+int main()
+{
+    std::cout << "testing build" << std::endl;
+
+    return 0;
+}
